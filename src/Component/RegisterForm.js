98b1e9f22746bc0e0bc7f368fa5cd5b89ab28a18
curatorRaj <1828043@kiit.ac.in>
2020-10-11 02:59:58 +0530

    added _redirects

--- src/Component/RegisterForm.js
+++ src/Component/RegisterForm.js
@@ -1,4 +1,4 @@
-import React,{useState,useEffect} from "react";
+import React,{useState} from "react";
 import axios from 'axios';
 import loginImage from '../login.svg'
 import registerImage from '../login.svg'
